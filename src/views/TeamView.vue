3f91e6ff39d7e78959bfe1ab7ef8c1c0fa777621
Prasenjit Das <prasun.das.89@gmail.com>
2023-11-05 13:58:18 -0800

    fleshed out Team data

--- src/views/TeamView.vue
+++ src/views/TeamView.vue
@@ -2,23 +2,25 @@
   <div>
     <h1>Our Team</h1>
     <ul>
-      <li v-for="member in team" :key="member.id">
+      <WelcomeItem v-for="member in team" :key="member.id">
         <h2>{{ member.name }}</h2>
-        <p>{{ member.position }}</p>
-        <p>{{ member.bio }}</p>
-      </li>
+        <p>{{ member.role }}</p>
+        <p>{{ member.location }}</p>
+      </WelcomeItem>
     </ul>
   </div>
 </template>
 
 <script>
 import teamData from '@/stores/team.json';
+import WelcomeItem from '@/components/WelcomeItem.vue';
 
 export default {
-  data() {
-    return {
-      team: teamData,
-    };
-  },
+    data() {
+        return {
+            team: teamData,
+        };
+    },
+    components: { WelcomeItem }
 };
 </script>
