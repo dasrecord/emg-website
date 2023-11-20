764a61bef1761a00c4d21af8d663d68f0d285234
Prasenjit Das <prasun.das.89@gmail.com>
2023-11-20 01:45:17 -0800

    .replace with <br>

--- src/main.js
+++ src/main.js
@@ -20,3 +20,27 @@ script.onerror = (error) => {
     console.error('Error loading timeline.js:', error)
 }
 document.body.appendChild(script)
+
+// Select the node that will be observed for mutations
+const targetNode = document.getElementById('app');
+
+// Options for the observer (which mutations to observe)
+const config = { attributes: true, childList: true, subtree: true };
+
+// Callback function to execute when mutations are observed
+const callback = function(mutationsList, observer) {
+    for(let mutation of mutationsList) {
+        if (mutation.type === 'childList') {
+            const timelineElements = document.querySelectorAll('.tl-timeline, .tl-timeline *');
+            timelineElements.forEach(el => {
+                el.style.fontFamily = 'Agency FB !important';
+            });
+        }
+    }
+};
+
+// Create an observer instance linked to the callback function
+const observer = new MutationObserver(callback);
+
+// Start observing the target node for configured mutations
+observer.observe(targetNode, config);
