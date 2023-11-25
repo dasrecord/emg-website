8ece0f8a584e767876d7828709a752270ce1bbc7
Prasenjit Das <prasun.das.89@gmail.com>
2023-11-25 00:22:13 -0800

    internal timeline links

--- src/views/InternalView.vue
+++ src/views/InternalView.vue
@@ -1,8 +1,8 @@
 <template>
-  <div v-if="artist">
+  <div v-if="artist" class="artist">
     <h1>{{ artist.artist_alias }}</h1>
-    <div id="timeline"></div>
   </div>
+  <div id="timeline"></div>
 </template>
 
 <script>
@@ -45,7 +45,38 @@ export default {
       if (this.artist && this.artist.management_timeline) {
         console.log(this.artist.management_timeline);
         this.$nextTick(() => {
-          const options = {} // replace with actual options if any
+          const options = {
+            width: window.innerWidth,
+            base_class: 'tl-timeline',
+            initial_zoom: 2,
+            start_at_end: true,
+            timenav_position: 'top',
+            default_bg_color: getComputedStyle(document.documentElement).getPropertyValue('--color-background').trim(),
+            font: 'default',
+            // other options...
+            scale_factor: 2,
+            // hash_bookmark: true,
+            // debug: false,
+            // is_embed: false,
+            // optimal_tick_width: 100,
+            // timenav_height: 150,
+            // timenav_height_percentage: 25,
+            // timenav_mobile_height_percentage: 40,
+            // timenav_height_min: 150,
+            // marker_height_min: 30,
+            // marker_width_min: 100,
+            // marker_padding: 5,
+            // start_at_slide: 0,
+            // menubar_height: 0,
+            // use_bc: false,
+            // duration: 1000,
+            // ease: TL.Ease.easeInOutQuint,
+            // dragging: true,
+            // trackResize: true,
+            // slide_padding_lr: 100,
+            // slide_default_fade: '0%',
+            // language: 'en'
+          }
           const timeline = new TL.Timeline('timeline', this.artist.management_timeline, options);
         });
       }
