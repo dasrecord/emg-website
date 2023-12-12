ee3eb668855445c03b8d39719b7e5c229024888e
Prasenjit Das <prasun.das.89@gmail.com>
2023-12-12 13:18:13 -0800

    Update iframe style and height in PlacementsView.vue

--- src/views/PlacementsView.vue
+++ src/views/PlacementsView.vue
@@ -1,7 +1,8 @@
 <template>
     <iframe
+      style="border-radius:25px" 
       src="https://open.spotify.com/embed/playlist/0Lu43931sOSst0v0gsl1jY?utm_source=generator&theme=0"
-      width="100%" height="800px" frameBorder="0" allowfullscreen="" 
+      width="100%" height="600px" frameBorder="" allowfullscreen="" 
       allow="autoplay; clipboard-write; encrypted-media; fullscreen; 
       picture-in-picture" loading="lazy">
     </iframe>
