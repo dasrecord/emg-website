66f91d396f54bed86da37c671062bea1c2984847
Prasenjit Das <prasun.das.89@gmail.com>
2023-12-12 11:47:08 -0800

    switch from .web.app to .com

--- generate_links.js
+++ generate_links.js
@@ -3,7 +3,7 @@ const roster = require('./src/stores/roster.json');
 
 
 const generateInternalLinks = (roster) => {
-    const internal_domain = 'https://evolvedmusicgroup.web.app/internal/';
+    const internal_domain = 'https://evolvedmusicgroup.com/internal/';
     let links = '';
 
     roster.forEach(artist => {
@@ -15,7 +15,7 @@ const generateInternalLinks = (roster) => {
     return links;
 }
 const generateExternalLinks = (paths) => {
-    const external_domain = 'https://evolvedmusicgroup.web.app/';
+    const external_domain = 'https://evolvedmusicgroup.com/';
     let links = '';
     paths.forEach(paths => {
         links += `${external_domain}${paths}\n`;
@@ -26,16 +26,20 @@ const generateExternalLinks = (paths) => {
 
 const header_text = "EVOLVED MUSIC GROUP\n\n"
 const internal_description_a = "INTERNAL MANAGEMENT TIMELINE FOLDER\n";
-const internal_folder = "https://evolvedmusicgroup.web.app/internal-timelines\n\n";
+const internal_folder = "https://evolvedmusicgroup.com/internal-timelines\n\n";
 const internal_description_b = "INTERNAL MANAGEMENT ACCOUNTABILITY LINKS\n"
 const final_internal_text = header_text + internal_description_a + internal_folder + internal_description_b + generateInternalLinks(roster);
 fs.writeFileSync('EMG_INTERNAL_MANAGEMENT_LINKS.txt', final_internal_text);
 
 
 const paths = [
-    'slack',
-    'discord',
-    'twitch-assets',
+    'about',
+    'consultacny',
+    'intake',
+    'demos',
+    'team',
+    'roster',
+    'placements',
     'contact',
     'bookings',
     'publishing-contract',
@@ -43,8 +47,15 @@ const paths = [
     'q&a',
     'a&r-meeting',
     'scouting-form',
+    'facebook',
+    'instagram',
+    'linkedin',
+    'youtube',
+    'slack',
+    'discord',
+    'twitch-assets',
 ]
-const external_description = "IMPORTANT LINKS\n\n";
+const external_description = "IMPORTANT LINKS\n";
 const final_external_text = header_text + external_description + generateExternalLinks(paths);
 fs.writeFileSync('EMG_IMPORTANT_EXTERNAL_LINKS.txt', final_external_text);
 
