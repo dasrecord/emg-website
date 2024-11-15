a1f5ad60d81f2b8cc76131ba41f0085de7716fb9
Prasenjit Das <prasun.das.89@gmail.com>
2024-11-15 13:35:32 -0800

    Add 2025 Songwriting Camps schedule with locations and dates

--- src/views/SongwritingCampsView.vue
+++ src/views/SongwritingCampsView.vue
@@ -1,21 +1,89 @@
 <template>
   <div class="camps">
     <h1>Songwriting Camps</h1>
+    <h1>2025</h1>
+
     <div class="camp">
       <h2>Texas</h2>
+      <h3>USA</h3>
       <h3>March 1st-6th, 2025</h3>
       <p>Tickets Coming Soon</p>
       <!-- <a href=""> Buy Passes</a> -->
     </div>
     <div class="camp">
+      <h2>Nashville</h2>
+      <h3>USA</h3>
+      <h3>March 17th - 20th, 2025</h3>
+      <p>Tickets Coming Soon</p>
+      <!-- <a href=""> Buy Passes</a> -->
+    </div>
+    <div class="camp">
+      <h2>Los Angeles</h2>
+      <h3>USA</h3>
+      <h3>March 21st - 23rd, 2025</h3>
+      <p>Tickets Coming Soon</p>
+      <!-- <a href=""> Buy Passes</a> -->
+    </div>
+    <div class="camp">
       <h2>Miami</h2>
+      <h3>USA</h3>
+      <h3>March 24th - 27th, 2025</h3>
+      <p>Tickets Coming Soon</p>
+      <!-- <a href=""> Buy Passes</a> -->
+    </div>
+    <div class="camp">
+      <h2>New York</h2>
+      <h3>USA</h3>
+      <h3>April 1st - 6th, 2025</h3>
+      <p>Tickets Coming Soon</p>
+      <!-- <a href=""> Buy Passes</a> -->
+    </div>
+    <div class="camp">
+      <h2>Atlanta</h2>
+      <h3>USA</h3>
+      <h3>April 8th - 13th, 2025</h3>
+      <p>Tickets Coming Soon</p>
+      <!-- <a href=""> Buy Passes</a> -->
+    </div>
+    <div class="camp">
+      <h2>Toronto</h2>
+      <h3>Canada</h3>
+      <h3>April 15th - 17th, 2025</h3>
+      <p>Tickets Coming Soon</p>
+      <!-- <a href=""> Buy Passes</a> -->
+    </div>
+    <div class="camp">
+      <h2>London</h2>
+      <h3>UK</h3>
       <h3>April 18th - 21st, 2025</h3>
       <p>Tickets Coming Soon</p>
       <!-- <a href=""> Buy Passes</a> -->
     </div>
     <div class="camp">
       <h2>Ibiza</h2>
+      <h3>Spain</h3>
       <h3>April 19th - 22nd, 2025</h3>
+      <p>Tickets Coming Soon</p>
+      <!-- <a href=""> Buy Passes</a> -->
+    </div>
+    <div class="camp">
+      <h2>Berlin</h2>
+      <h3>Germany</h3>
+      <h3>May 10th - 13th, 2025</h3>
+      <p>Tickets Coming Soon</p>
+      <!-- <a href=""> Buy Passes</a> -->
+    </div>
+    <div class="camp">
+      <h2>Brighton</h2>
+      <h3>UK</h3>
+      <h3>May 24th - 27th, 2025</h3>
+      <p>Tickets Coming Soon</p>
+      <!-- <a href=""> Buy Passes</a> -->
+    </div>
+    <div class="camp">
+      <h2>London</h2>
+      <h3>UK</h3>
+      <h3>May 28th - June 1st, 2025</h3>
       <p>Tickets Coming Soon</p>
       <!-- <a href=""> Buy Passes</a> -->
     </div>
